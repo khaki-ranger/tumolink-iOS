1a5a33c4efe5a92956a6a547a15f14102e91d645
Terashima <yamanogomora@gmail.com>
2019-02-11 13:07:06 +0900

    Initial Commit

---
+++ tumolink-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tumolink-iOS
+//
+//  Created by 寺島 洋平 on 2019/02/11.
+//  Copyright © 2019年 YoheiTerashima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
